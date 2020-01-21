20a894ef7046aa9a49bccdbe32c66e9838a3a44e
Johannes Törmänen <johannes.tormanen@hotmail.fi>
2020-01-21 14:25:09 +0200

    Oven ohjaus harjoitus

--- ovi.py
+++ ovi.py
@@ -1,5 +1,5 @@
-oviKiinni = False
-oviLukittu = False
+oviKiinni = True
+oviLukittu = True
 oviTila = ""
 
 if oviKiinni == False and oviLukittu == False:
@@ -10,8 +10,42 @@ elif oviKiinni == True and oviLukittu == True:
     oviTila = "Kiinni ja Lukittu"
     
 print("ovi on " + oviTila)
-userInput = int(input("Mitäs tehdään? sulje ovi = 1, sulje ja lukitse = 2 " ))
-if userInput == 1:
-    oviTila = "Kiinni ja Avoinna"
-    print("ovi on " + oviTila)
+if oviKiinni == True and oviLukittu == True:
+    userInput = int(input("Mitäs tehdään? Avaa lukko ja ovi = 1, avaa vain lukko = 2"))
+    if userInput == 1:
+        oviTila = "Auki ja Avoinna"   
+        oviKiinni = False
+        oviLukittu = False
+        print("ovi on " + oviTila)
+    elif userInput == 2:
+        oviTila = "Kiinni ja Avoinna"   
+        oviKiinni = True
+        oviLukittu = False
+        print("ovi on " + oviTila)
     
+if oviKiinni == False and oviLukittu == False:
+    userInput = int(input("Ovi on auki, mitäs tehdään? Sulje ovi = 1, sulje ja lukitse = 2"))
+    if userInput == 1:
+        oviTila = "Kiinni ja Avoinna"   
+        oviKiinni = True
+        oviLukittu = False
+        print("ovi on " + oviTila)
+    elif userInput == 2:
+        oviTila = "Kiinni ja lukittu"   
+        oviKiinni = True
+        oviLukittu = True
+        print("ovi on " + oviTila)
+        
+if oviKiinni == True and oviLukittu == False:
+    oviTila = "Kiinni ja Avoinna"
+    userInput = int(input("Mitäs tehdään? Avaa ovi = 1, lukitse ovi = 2"))
+    if userInput == 1:
+        oviTila = "Auki ja Avoinna"   
+        oviKiinni = False
+        oviLukittu = False
+        print("ovi on " + oviTila)
+    elif userInput == 2:
+        oviTila = "Kiinni ja Lukittu"   
+        oviKiinni = True
+        oviLukittu = True
+        print("ovi on " + oviTila)
